062c9c3579a4253404178dd878d1c4d1dc67c953
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2024-12-29 01:56:40 -0500

    Solved Part 1 very naievely

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    part_1()
+}
+
+fn part_1() {
+    let contents = fs::read_to_string("./data/part-1.txt")
+        .expect("Should have been able to read the file");
+    let chars = contents.chars();
+    let mut potions = 0;
+    for char in chars {
+        match char {
+            'B' => potions +=1,
+            'C' => potions +=3,
+            _ => potions += 0,
+        }
+    }
+
+    println!("potions: {potions}");
 }
